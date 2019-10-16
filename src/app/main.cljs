6d124fd0ae5b64ee2afe49a9121a11c92b037a13
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-10-15 21:24:11 -0700

    Finished minimal demo

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,11 +1,44 @@
-(ns app.main)
+(ns app.main
+  (:require ["@blueprintjs/core" :as bp]
+            [reagent.core :as r]))
 
-(def a 1)
+;; Util functions
 
-(defonce b 2)
+(defn console-greeting []
+  (println "[greeting]" (js/Date.) "it works!"))
+
+(defonce dialog-open
+  (r/atom false))
+
+;; App
+
+(defn example-app []
+  [:<>
+   [:> bp/Button {:icon "eye-open" :text "Print greeting in console!" :onClick console-greeting}]
+   [:br][:br]
+   [:> bp/Button {:icon "chat" :text "Show dialog" :onClick #(swap! dialog-open false?)}]
+
+   ;; Dialog
+   [:> bp/Dialog {:icon "info-sign" :isOpen @dialog-open :title "Title here" :onClose #(reset! dialog-open false)}
+    [:div {:className bp/Classes.DIALOG_BODY}
+     [:p
+      [:strong "bla bla blah"]]
+     [:p "bla bla bla continues"]]
+    [:div {:className bp/Classes.DIALOG_FOOTER}
+     [:div {:className bp/Classes.DIALOG_FOOTER_ACTIONS}
+      [:> bp/Tooltip {:content "This button is hooked up to close the dialog."}
+       [:> bp/Button {:onClick #(reset! dialog-open false)} "Close"]]]]]
+   ])
+
+;; App initialization
+
+(defn mount-root []
+  (r/render [example-app] (.getElementById js/document "app")))
 
 (defn main! []
-  (println "[main]: loading"))
+  (mount-root)
+  (println "[core]: loading"))
 
 (defn reload! []
-  (println "[main] reloaded:" a b))
+  (mount-root)
+  (println "[core] reloaded"))
